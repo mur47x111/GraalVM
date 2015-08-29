d23222dca4c7d967f24de028c015204b1e8de395
mur47x111 <mur47x111@gmail.com>
2015-08-29 10:40:39 +0200

    inline util usage

--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/replacements/query/GetRootNameNode.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/replacements/query/GetRootNameNode.java
@@ -1,5 +1,9 @@
 package com.oracle.graal.hotspot.replacements.query;
 
+import jdk.internal.jvmci.hotspot.*;
+import jdk.internal.jvmci.meta.*;
+
+import com.oracle.graal.compiler.common.type.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.nodeinfo.*;
 import com.oracle.graal.nodes.*;
@@ -11,13 +15,16 @@ public final class GetRootNameNode extends GraalQueryNode {
     public static final NodeClass<GetRootNameNode> TYPE = NodeClass.create(GetRootNameNode.class);
 
     public GetRootNameNode() {
-        super(TYPE, CompilerDecisionUtil.STAMP_STRING);
+        super(TYPE, StampFactory.exactNonNull(HotSpotResolvedJavaType.fromClass(String.class)));
     }
 
     @Override
     public void onInlineICG(InstrumentationNode instrumentation, FixedNode position) {
-        String methodName = CompilerDecisionUtil.getMethodFullName(graph().method());
-        graph().replaceFixedWithFloating(this, CompilerDecisionUtil.createStringConstant(graph(), methodName));
+        ResolvedJavaMethod method = graph().method();
+        String root = method.getDeclaringClass().toJavaName() + "." + method.getName() + method.getSignature().toMethodDescriptor();
+        Constant constant = HotSpotObjectConstantImpl.forBoxedValue(Kind.Object, root);
+        ConstantNode constantNode = graph().unique(new ConstantNode(constant, stamp()));
+        graph().replaceFixedWithFloating(this, constantNode);
     }
 
     @NodeIntrinsic
